a740968fefc1a4bbeba971341db5d0c767eaef1f
Pete Lumbis <plumbis@cumulusnetworks.com>
2018-01-15 14:36:08 -0800

    correct copy

--- helper_scripts/config_oob_switch.sh
+++ helper_scripts/config_oob_switch.sh
@@ -5,7 +5,7 @@ echo "   Running config_oob_switch.sh"
 echo "#################################"
 sudo su
 
-copy /home/vagrant/bridge-untagged /etc/network/interfaces
+cp /home/vagrant/bridge-untagged /etc/network/interfaces
 ifreload -a
 
 echo "#################################"
