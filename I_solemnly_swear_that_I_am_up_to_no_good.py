6699ac3a8304f4a2c02a456053b36d4f89eb31b4
Pete Lumbis <plumbis@cumulusnetworks.com>
2018-01-20 17:54:55 -0800

    add dashes to break

--- I_solemnly_swear_that_I_am_up_to_no_good.py
+++ I_solemnly_swear_that_I_am_up_to_no_good.py
@@ -20,7 +20,7 @@ def run_command(host, command):
 def cli_commands():
     parser = argparse.ArgumentParser(description="A simple chaos-monkey like application using Cumulus NetQ")
 
-    parser.add_argument("-b", "break", choices=["mtu", "evpn", "bgp", "interface", "routerid"],
+    parser.add_argument("-b", "--break", choices=["mtu", "evpn", "bgp", "interface", "routerid"],
                         help="What to break?", required=True)
 
     parser.add_argument("-v", "--victims", help="How many hosts to break?", type=int, required=True)
