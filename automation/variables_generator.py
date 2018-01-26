fdeed930d81cbff3e2cdb9d11e0814786e2ebf2e
Pete Lumbis <plumbis@cumulusnetworks.com>
2018-01-26 07:53:09 -0800

    two-tier changes

--- automation/variables_generator.py
+++ automation/variables_generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pprint
 import yaml
-
+import copy
 # node:
 #     leaf01:
 #         interfaces:
@@ -26,59 +26,32 @@ import yaml
 #             bgp:
 #                 asn: 4200000100
 
-leaf_count = 128
-chassis_list = ["chassis01", "chassis02", "chassis03", "chassis04"]
-linecard_list = ["lc1-1", "lc1-2", "lc2-1", "lc2-2", "lc3-1", "lc3-2", "lc4-1", "lc4-2"]
-fabriccard_list = ["fc1-1", "fc2-1", "fc3-1", "fc4-1"]
+leaf_count = 32
+spine_count = 4
+host_count = 6
 
 inventory = {}
 subnet = "10.0.0."
 asn = 64512
+peer_output = []
 
-current_leaf = 1
+for leaf in range(1, leaf_count + 1):
+    v4_addr = subnet + str(leaf) + "/32"
 
-while current_leaf <= leaf_count:
-    v4_addr = subnet + str(current_leaf) + "/32"
-
-    inventory["leaf" + str("%02d" % current_leaf)] = {"interfaces": {"lo": v4_addr},
+    inventory["leaf" + str("%02d" % leaf)] = {"interfaces": {"lo": v4_addr},
                                                       "bgp" : {"asn": str(asn), "peers": ["swp1", "swp2", "swp3", "swp4"]}
                                                      }
-
-    current_leaf += 1
+    peer_output.append("swp" + str(leaf))
     asn += 1
 
-ip = current_leaf
+subnet = "10.255.255."
 
 
-for chassis in chassis_list:
-
-    for linecard in linecard_list:
-        hostname = chassis + "-" + linecard
-        v4_addr = subnet + str(ip) + "/32"
-        peers = []
-        for swp in range(1,17):
-            peers.append("swp" + str(swp))
-            peers.append("fp" + str(swp - 1))
-
-            inventory[hostname] = {"interfaces": {"lo": v4_addr},
-                                                      "bgp" : {"asn": str(asn), "peers": peers}
+for spine in range(1, spine_count + 1):
+    v4_addr = subnet + str(spine) + "/32"
+    inventory["spine" + str("%02d" % spine)] = {"interfaces": {"lo": v4_addr},
+                                                      "bgp" : {"asn": str(asn), "peers": copy.deepcopy(peer_output)}
                                                      }
-
-        ip += 1
-        asn += 1
-
-    for fabriccard in fabriccard_list:
-        hostname = chassis + "-" + fabriccard
-        v4_addr = subnet + str(ip) + "/32"
-        peers = []
-        for fp in range(0,32):
-            peers.append("fp" + str(fp))
-
-        inventory[hostname] = {"interfaces": {"lo": v4_addr},
-                                                      "bgp" : {"asn": str(asn), "peers": peers}
-                                                     }
-
-        ip += 1
 
     asn += 1
 
